df7461b3bad626c95b6d07b69a3c5c7c26bfc75b
Aaryaman-Singh-Patel <aarya192002@gmail.com>
2024-05-02 22:28:17 +0530

    new edit

--- A.java
+++ A.java
@@ -1,7 +1,13 @@
-class A
+public class A
       { 
+         void call()
+         {
+            System.out.println("Hello");
+         }
          public static void main(String []args)
          {
             System.out.println("Java file commited");
+            Main obj = new Main();
+            obj.call();
          }
        }
